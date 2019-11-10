ea83995d88fec2b3f1ffef51a2fc97ba4d51860d
AnaPavlov <anapavlovic94@yahoo.com>
2019-11-10 19:38:12 +0100

    Updates

---
+++ precache-manifest.4e4fefaf80a5480d2eefd372bc5961cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccee53be6c4539d9c1796042ef49e8b4",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "0bd25946ba346839bd29",
+    "url": "/recipes-app/static/css/main.15a5a968.chunk.css"
+  },
+  {
+    "revision": "6d98a7ff4c185b2d30cf",
+    "url": "/recipes-app/static/js/2.05c9f88f.chunk.js"
+  },
+  {
+    "revision": "0bd25946ba346839bd29",
+    "url": "/recipes-app/static/js/main.67e84754.chunk.js"
+  },
+  {
+    "revision": "ff74faa87fc3df8bb2d1",
+    "url": "/recipes-app/static/js/runtime-main.f4e82d55.js"
+  },
+  {
+    "revision": "2c22d30ba1d705a7f7eb2cf335198fa4",
+    "url": "/recipes-app/static/media/about.2c22d30b.jpg"
+  },
+  {
+    "revision": "7c1fd99f7aec267b09e10da9e9d61ac2",
+    "url": "/recipes-app/static/media/header.7c1fd99f.png"
+  },
+  {
+    "revision": "7c58267688bf3351e34669b16fcdea96",
+    "url": "/recipes-app/static/media/insta.7c582676.png"
+  },
+  {
+    "revision": "8a4053ac585059669df2949d165471f6",
+    "url": "/recipes-app/static/media/login-icon.8a4053ac.png"
+  },
+  {
+    "revision": "45290b3d607522ef3ee44f7d5376c86e",
+    "url": "/recipes-app/static/media/logo.45290b3d.png"
+  }
+]);
